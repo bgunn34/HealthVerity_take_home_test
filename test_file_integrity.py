6d5fd89e5cd38768a498fdba80e26b3f8e556ffc
Benjamin Gunn <ben_gunn@Benjamins-MacBook-Pro.local>
2022-02-08 14:43:03 -0500

    wrapped up testing document.

--- test_file_integrity.py
+++ test_file_integrity.py
@@ -1,48 +1,84 @@
 import pytest
 import pandas as pd
 
-## specifying dtypes at load will produce an error if data is included that
-## is incompatable with that type.
-dtypes = {
-	'patient_id': str,
-	'claim_id': int,
-	'diagnosis_code': str,
-	'procedure_code': int,
-	'date_service': str,
-	'date_received': str,
-}
-df = pd.read_csv('data/sample_claims.csv', dtype=dtypes)
+
+df = pd.read_csv('data/sample_claims.csv')
 valid_cpts = pd.read_csv('data/valid_cpt_codes.csv')
 valid_icds = pd.read_csv('data/valid_icd_10_codes.csv')
 
 def test_table_def():
-	# checks that the claims file matches the provided 
-	pass
+	# checks that the claims file matches the provided table definition
+	cols = [
+		'patient_id',
+		'claim_id',
+		'diagnosis_codes',
+		'procedure_code',
+		'date_service',
+		'date_received',
+	]
+	assert list(df.columns) == cols
 
 
 def test_cpt_codes():
 	# checks that all cpt codes in the claims data are valid
-	pass
+	assert df['procedure_code'].isin(valid_cpts['code']).all()
 
 
 def test_icd_codes():
 	# checks that all icd codes in the claims data are valid
-	pass
+	icd_lists = df['diagnosis_codes'].str.split('^')
+	icds = icd_lists.explode()
+	icds = icds.str.replace('.', '', regex=False)
+	assert icds.isin(valid_icds['code']).all()
 
 
-def test_date_format():
+def test_service_date_na():
+	# checks to make sure all claims have service dates.
+	assert find_na('date_service') == 0
+
+
+def test_received_date_na():
+	# checks to make sure all claims have received dates.
+	assert find_na('date_received') == 0
+
+
+def test_serv_date_format():
 	# checks if all date values are in YYYY-MM-DD format
-	pass
+	# the to_datetime call will error if the format is incorrect.
+	serv_dts = df.loc[df['date_service'].notna(), 'date_service']
+	serv_dts = pd.to_datetime(serv_dts, format='%Y-%m-%d', errors='raise')
+	assert True
+
+
+def test_rec_date_format():
+	# checks if all date values are in YYYY-MM-DD format
+	# the to_datetime call will error if the format is incorrect.
+	rec_dts = df.loc[df['date_received'].notna(), 'date_received']
+	rec_dts = pd.to_datetime(rec_dts, format='%Y-%m-%d', errors='raise')
+	assert True
 
 
 def test_date_values():
 	# checks if all service dates < received dates.
-	pass
+	serv_dts = df.loc[df['date_service'].notna(), 'date_service']
+	serv_dts = pd.to_datetime(serv_dts, format='%Y-%m-%d', errors='raise')
+
+	rec_dts = df.loc[df['date_received'].notna(), 'date_received']
+	rec_dts = pd.to_datetime(rec_dts, format='%Y-%m-%d', errors='raise')
+
+	assert (serve_dts < rec_dts).all()
+
+
+def test_claim_id_na():
+	# checks to make sure all claims have claim ids.
+	assert find_na('claim_id') == 0
 
 
 def test_claim_id():
-	# checks if all claim ids are numeric.
-	pass
+	# checks if all claim ids are ints. filters na's since we're testing those 
+	# separately.
+	claim_ids = df.loc[df['claim_id'].notna(), 'claim_id']
+	assert claim_ids.eq(claim_ids.astype(int)).all()
 
 
 def test_patient_na():
@@ -50,24 +86,9 @@ def test_patient_na():
 	assert find_na('patient_id') == 0
 
 
-def test_claim_id_na():
-	# checks to make sure all claims have claim ids.
-	assert find_na('claim_id')
-
-
 def test_procedure_code_na():
 	# checks to make sure all claims have procedure codes.
-	assert find_na('procedure_code')
-
-
-def test_service_date_na():
-	# checks to make sure all claims have service dates.
-	assert find_na('date_service')
-
-
-def test_received_date_na():
-	# checks to make sure all claims have received dates.
-	assert find_na('date_received')
+	assert find_na('procedure_code') == 0
 
 
 def find_na(col):
